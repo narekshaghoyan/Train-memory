031952b0c2f4e5d7b7811c1436886b397942d745
Narek Shaghoyan <narek.shaghoyan@tumo.org>
2024-06-24 15:16:07 +0400

    Add file structure, adding some click functions to change colors. add some styles. And upload .ico

---
+++ public/assets/js/js.examples.js
@@ -0,0 +1,19 @@
+/* 
+  Click listener
+
+  let el = document.getElementById('memory-game')
+  el.addEventListener('click', myFunction)
+  function myFunction() {
+    console.log('clicked')
+  }
+*/
+
+/* 
+  Hover listener
+
+  let ela = document.getElementById('memory-game')
+  ela.addEventListener('mouseover', myFunction)
+  function myFunction() {
+      console.log('hovering...')
+    }
+*/
